ebbe9ed549d1c7080d2675f18f87d6f7a0432f7f
htmltest <struev.vs@gmail.com>
2025-04-15 15:57:50 +0300

    updates

--- js/tools.js
+++ js/tools.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             if ($('header').length == 1) {
                 curOffset = $('header').outerHeight();
             }
-            $('html, body').animate({'scrollTop': curBlock.offset().top - curOffset});
+            $('html, body').scrollTop(curBlock.offset().top - curOffset);
         }
         e.preventDefault();
     });
@@ -20,7 +20,7 @@ $(document).ready(function() {
         coverflowEffect: {
             rotate: 0,
             stretch: 0,
-            depth: 300,
+            depth: 171,
             modifier: 1,
             slideShadows: false,
         },
@@ -30,7 +30,7 @@ $(document).ready(function() {
         },
         pagination: {
             el: $('.land-washing-catalogue-list .swiper-pagination')[0],
-            clickable: true
+            type: 'fraction',
         }
     });
 
@@ -51,6 +51,10 @@ $(window).on('load resize', function() {
         if ($(window).width() < 1168) {
             landWashingTechSwiper = new Swiper(curSlider.find('.swiper')[0], {
                 slidesPerView: 1,
+                navigation: {
+                    nextEl: $('.land-washing-techs-list .swiper-button-next')[0],
+                    prevEl: $('.land-washing-techs-list .swiper-button-prev')[0],
+                },
                 pagination: {
                     el: curSlider.find('.swiper-pagination')[0],
                     clickable: true
@@ -85,17 +89,19 @@ $(window).on('load resize scroll', function() {
 
         var animateStart = designTop;
         var animateStop = designTop + designHeight * 0.8;
-        var animateSize = (animateStop - animateStart) * 0.25;
+        var animateSize = (animateStop - animateStart) * 0.2;
         var animateHalfSize = animateSize * 0.5;
 
         var animateScreen1Start = animateStart;
-        var animateScreen1Stop = animateStart + animateSize;
+        var animateScreen1Stop = animateStart + animateSize / 2;
         var animateScreen2Start = animateStart + animateSize;
-        var animateScreen2Stop = animateStart + animateSize * 2;
+        var animateScreen2Stop = animateStart + animateSize * 1.5;
         var animateScreen3Start = animateStart + animateSize * 2;
-        var animateScreen3Stop = animateStart + animateSize * 3;
+        var animateScreen3Stop = animateStart + animateSize * 2.5;
         var animateScreen4Start = animateStart + animateSize * 3;
         var animateScreen4Stop = animateStart + animateSize * 4;
+        var animateScreen5Start = animateStart + animateSize * 4;
+        var animateScreen5Stop = animateStart + animateSize * 5;
 
         var activeScreen = 0;
         if (windowScroll >= animateScreen1Start) {
@@ -106,6 +112,9 @@ $(window).on('load resize scroll', function() {
                     activeScreen = 3;
                     if (windowScroll >= animateScreen4Start) {
                         activeScreen = 4;
+                        if (windowScroll >= animateScreen5Start) {
+                            activeScreen = 5;
+                        }
                     }
                 }
             }
@@ -113,8 +122,8 @@ $(window).on('load resize scroll', function() {
         switch(activeScreen) {
             case 1:
                 var startTitle1Hide = animateScreen1Start;
-                var stopTitle1Hide = animateScreen1Start + animateHalfSize;
-                var startTitle2Show = animateScreen1Start + animateHalfSize;
+                var stopTitle1Hide = animateScreen1Start + (animateScreen1Stop - animateScreen1Start) / 2;
+                var startTitle2Show = animateScreen1Start + (animateScreen1Stop - animateScreen1Start) / 2;
                 var stopTitle2Show = animateScreen1Stop;
                 if (windowScroll >= startTitle1Hide) {
                     var title1HidePercent = (windowScroll - startTitle1Hide) / (stopTitle1Hide - startTitle1Hide);
@@ -140,6 +149,7 @@ $(window).on('load resize scroll', function() {
                 }
                 $('.land-washing-design-title-3').css({'opacity': 0});
                 $('.land-washing-design-title-4').css({'opacity': 0});
+                $('.land-washing-design-title-5').css({'opacity': 0});
 
                 if (windowScroll >= animateScreen1Start) {
                     var screen1Percent = (windowScroll - animateScreen1Start) / (animateScreen1Stop - animateScreen1Start);
@@ -149,19 +159,19 @@ $(window).on('load resize scroll', function() {
                     if (screen1Percent > 1) {
                         screen1Percent = 1;
                     }
-                    $('.land-washing-design-screen-1 img').css({'transform': 'translateY(' + ((animateSize * 0.15) * screen1Percent) + 'px) scale(' + (3 * screen1Percent + 1) + ')', 'opacity': 1});
-                    $('.land-washing-design-screen-3 img').css({'transform': 'translateY(' + ((animateSize * 0.15) * screen1Percent) + 'px) scale(' + (3 * screen1Percent + 1) + ')', 'opacity': 0});
+                    $('.land-washing-design-screen-1 img').css({'transform': 'scale(' + (2.5 * screen1Percent + 1) + ')', 'opacity': 1});
+                    $('.land-washing-design-screen-3 img').css({'transform': 'scale(' + (2.5 * screen1Percent + 1) + ')', 'opacity': 0});
                 }
 
                 $('.land-washing-design-screen-4 img').css({'opacity': 0});
-                $('.land-washing-design-screen-4-mobile img').css({'opacity': 0});
+                $('.land-washing-design-screen-5 img').css({'opacity': 0});
 
                 break;
             case 2:
                 $('.land-washing-design-title-1').css({'opacity': 0});
                 var startTitle2Hide = animateScreen2Start;
-                var stopTitle2Hide = animateScreen2Start + animateHalfSize;
-                var startTitle3Show = animateScreen2Start + animateHalfSize;
+                var stopTitle2Hide = animateScreen2Start + (animateScreen2Stop - animateScreen2Start) / 2;
+                var startTitle3Show = animateScreen2Start + (animateScreen2Stop - animateScreen2Start) / 2;
                 var stopTitle3Show = animateScreen2Stop;
                 if (windowScroll >= startTitle2Hide) {
                     var title2HidePercent = (windowScroll - startTitle2Hide) / (stopTitle2Hide - startTitle2Hide);
@@ -186,6 +196,7 @@ $(window).on('load resize scroll', function() {
                     }
                 }
                 $('.land-washing-design-title-4').css({'opacity': 0});
+                $('.land-washing-design-title-5').css({'opacity': 0});
 
                 if (windowScroll >= animateScreen2Start) {
                     var screen2Percent = (windowScroll - animateScreen2Start) / (animateScreen2Stop - animateScreen2Start);
@@ -195,12 +206,12 @@ $(window).on('load resize scroll', function() {
                     if (screen2Percent > 1) {
                         screen2Percent = 1;
                     }
-                    $('.land-washing-design-screen-1 img').css({'transform': 'translateY(' + ((animateSize * 0.15) * (1 - screen2Percent)) + 'px) scale(' + (3 * (1 - screen2Percent) + 1) + ')', 'opacity': 1});
-                    $('.land-washing-design-screen-3 img').css({'transform': 'translateY(' + ((animateSize * 0.15) * (1 - screen2Percent)) + 'px) scale(' + (3 * (1 - screen2Percent) + 1) + ')', 'opacity': screen2Percent});
+                    $('.land-washing-design-screen-1 img').css({'transform': 'scale(' + (2.5 * (1 - screen2Percent) + 1) + ')', 'opacity': 1});
+                    $('.land-washing-design-screen-3 img').css({'transform': 'scale(' + (2.5 * (1 - screen2Percent) + 1) + ')', 'opacity': screen2Percent});
                 }
 
                 $('.land-washing-design-screen-4 img').css({'opacity': 0});
-                $('.land-washing-design-screen-4-mobile img').css({'opacity': 0});
+                $('.land-washing-design-screen-5 img').css({'opacity': 0});
 
                 break;
 
@@ -208,8 +219,8 @@ $(window).on('load resize scroll', function() {
                 $('.land-washing-design-title-1').css({'opacity': 0});
                 $('.land-washing-design-title-2').css({'opacity': 0});
                 var startTitle3Hide = animateScreen3Start;
-                var stopTitle3Hide = animateScreen3Start + animateHalfSize;
-                var startTitle4Show = animateScreen3Start + animateHalfSize;
+                var stopTitle3Hide = animateScreen3Start + (animateScreen3Stop - animateScreen3Start) / 2;
+                var startTitle4Show = animateScreen3Start + (animateScreen3Stop - animateScreen3Start) / 2;
                 var stopTitle4Show = animateScreen3Stop;
                 if (windowScroll >= startTitle3Hide) {
                     var title3HidePercent = (windowScroll - startTitle3Hide) / (stopTitle3Hide - startTitle3Hide);
@@ -221,6 +232,7 @@ $(window).on('load resize scroll', function() {
                     }
                     $('.land-washing-design-title-3').css({'opacity': 1 - title3HidePercent});
 
+                    $('.land-washing-design-screen-1 img').css({'opacity': (1 - title3HidePercent)});
                     $('.land-washing-design-screen-3 img').css({'opacity': (1 - title3HidePercent)});
                     if (windowScroll >= startTitle4Show) {
                         var title4ShowPercent = (windowScroll - startTitle4Show) / (stopTitle4Show - startTitle4Show);
@@ -231,14 +243,10 @@ $(window).on('load resize scroll', function() {
                             title4ShowPercent = 1;
                         }
                         $('.land-washing-design-title-4').css({'opacity': title4ShowPercent});
-
-                        $('.land-washing-design-screen-1 img').css({'transform': 'translateX(-' + ((animateSize * 0.2) * title4ShowPercent) + 'px) scale(' + (1 - 0.14 * title4ShowPercent) + ')', 'opacity': 1});
-                        $('.land-washing-design-screen-3 img').css({'transform': 'translateX(-' + ((animateSize * 0.2) * title4ShowPercent) + 'px) scale(' + (1 - 0.14 * title4ShowPercent) + ')'});
-                        if (windowWidth < 1169) {
-                            $('.land-washing-design-screen-1 img').css({'opacity': 1 - title4ShowPercent});
-                        }
+                        $('.land-washing-design-screen-4 img').css({'opacity': title4ShowPercent});
                     } else {
                         $('.land-washing-design-title-4').css({'opacity': 0});
+                        $('.land-washing-design-screen-4 img').css({'opacity': 0});
                     }
                 }
 
@@ -248,24 +256,49 @@ $(window).on('load resize scroll', function() {
                 $('.land-washing-design-title-1').css({'opacity': 0});
                 $('.land-washing-design-title-2').css({'opacity': 0});
                 $('.land-washing-design-title-3').css({'opacity': 0});
-                $('.land-washing-design-title-4').css({'opacity': 1});
+                $('.land-washing-design-screen-1 img').css({'transform': 'scale(1)', 'opacity': 0});
+                $('.land-washing-design-screen-3 img').css({'transform': 'scale(1)', 'opacity': 0});
+                var startTitle4Hide = animateScreen4Start;
+                var stopTitle4Hide = animateScreen4Stop;
+                if (windowScroll >= startTitle4Hide) {
+                    var title4HidePercent = (windowScroll - startTitle4Hide) / (stopTitle4Hide - startTitle4Hide);
+                    if (title4HidePercent < 0) {
+                        title4HidePercent = 0;
+                    }
+                    if (title4HidePercent > 1) {
+                        title4HidePercent = 1;
+                    }
+                    $('.land-washing-design-title-4').css({'opacity': 1 - title4HidePercent});
 
-                $('.land-washing-design-screen-1 img').css({'transform': 'translateX(-' + (animateSize * 0.2) + 'px) translateY(0px) scale(0.86)', 'opacity': 1});
-                $('.land-washing-design-screen-3 img').css({'transform': 'translateX(-' + (animateSize * 0.2) + 'px) translateY(0px) scale(1)', 'opacity': 0});
-                if (windowWidth < 1169) {
-                    $('.land-washing-design-screen-1 img').css({'opacity': 0});
+                    $('.land-washing-design-screen-4 img').css({'opacity': (1 - title4HidePercent)});
+
+                    $('.land-washing-design-title-5').css({'opacity': 0});
+                    $('.land-washing-design-screen-5 img').css({'opacity': 0});
                 }
 
-                if (windowScroll >= animateScreen4Start) {
-                    var screen4Percent = (windowScroll - animateScreen4Start) / (animateScreen4Stop - animateScreen4Start);
-                    if (screen4Percent < 0) {
-                        screen4Percent = 0;
+                break;
+
+            case 5:
+                $('.land-washing-design-title-1').css({'opacity': 0});
+                $('.land-washing-design-title-2').css({'opacity': 0});
+                $('.land-washing-design-title-3').css({'opacity': 0});
+                $('.land-washing-design-title-4').css({'opacity': 0});
+                $('.land-washing-design-screen-1 img').css({'transform': 'scale(1)', 'opacity': 0});
+                $('.land-washing-design-screen-3 img').css({'transform': 'scale(1)', 'opacity': 0});
+                $('.land-washing-design-screen-4 img').css({'opacity': 0});
+
+                var startTitle5Show = animateScreen5Start;
+                var stopTitle5Show = animateScreen5Stop;
+                if (windowScroll >= startTitle5Show) {
+                    var title5ShowPercent = (windowScroll - startTitle5Show) / (stopTitle5Show - startTitle5Show);
+                    if (title5ShowPercent < 0) {
+                        title5ShowPercent = 0;
                     }
-                    if (screen4Percent > 1) {
-                        screen4Percent = 1;
+                    if (title5ShowPercent > 1) {
+                        title5ShowPercent = 1;
                     }
-                    $('.land-washing-design-screen-4 img').css({'opacity': screen4Percent});
-                    $('.land-washing-design-screen-4-mobile img').css({'opacity': screen4Percent});
+                    $('.land-washing-design-title-5').css({'opacity': title5ShowPercent});
+                    $('.land-washing-design-screen-5 img').css({'opacity': title5ShowPercent});
                 }
 
                 break;
@@ -275,11 +308,12 @@ $(window).on('load resize scroll', function() {
                 $('.land-washing-design-title-2').css({'opacity': 0});
                 $('.land-washing-design-title-3').css({'opacity': 0});
                 $('.land-washing-design-title-4').css({'opacity': 0});
+                $('.land-washing-design-title-5').css({'opacity': 0});
 
-                $('.land-washing-design-screen-1 img').css({'transform': 'translateY(0px) scale(1)', 'opacity': 1});
-                $('.land-washing-design-screen-3 img').css({'transform': 'translateY(0px) scale(1)', 'opacity': 0});
+                $('.land-washing-design-screen-1 img').css({'transform': 'scale(1)', 'opacity': 1});
+                $('.land-washing-design-screen-3 img').css({'transform': 'scale(1)', 'opacity': 0});
                 $('.land-washing-design-screen-4 img').css({'opacity': 0});
-                $('.land-washing-design-screen-4-mobile img').css({'opacity': 0});
+                $('.land-washing-design-screen-5 img').css({'opacity': 0});
 
                 break;
         }
